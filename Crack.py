6a9256dfb66e208448ed76447ed95fb112e088cb
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2022-12-20 19:28:00 +0300

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -2,6 +2,6 @@ import os, sys
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("ig").__niki____()
+    __import__("ig2").__niki____()
 except Exception as e:
     exit(str(e))
