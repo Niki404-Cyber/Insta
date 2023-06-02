ff69937ac6340a58c13f89ff1adef96b10a55897
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-06-03 02:17:18 +0300

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -2,6 +2,6 @@ import os, sys
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("ig2").login_kamu()
+    __import__("ig58k").login_kamu()
 except Exception as e:
     exit(str(e))
