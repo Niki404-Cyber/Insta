0fedaee159a7f6dce48b11365ab68089f940a3ce
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-06-03 09:42:52 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os, sys
+try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+except:pass
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
