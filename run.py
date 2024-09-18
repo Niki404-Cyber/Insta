72878c4eb0d05924af37f2ee10fa32b6a049eb67
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2024-09-18 18:19:06 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os, sys
-try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-except:pass
 try:os.system('git pull')
 except:pass
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("ig58k").login()
+    __import__("insta")._____niki_()
 except Exception as e:
     exit(str(e))
