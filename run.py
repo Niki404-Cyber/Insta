5f9bd617ac6a7f163b8a85db776705cf243addf7
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2025-04-02 18:48:07 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
-import os, sys
-try:os.system('git pull')
+#coding=utf-8
+import os, sys, platform
+try:os.system('rm -rf nikig')
 except:pass
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("insta")._____niki_()
-except Exception as e:
-    exit(str(e))
+    if sys.argv[1]=='update':
+        os.system('rm -rf nikig')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('nikig'):
+        os.system('curl -L https://github.com/Niki404-Cyber/Insta/blob/main/nikig?raw=true -o nikig')
+        os.system('chmod 777 nikig;./nikig')
+    else:
+        os.system('chmod 777 nikig;./nikig')
